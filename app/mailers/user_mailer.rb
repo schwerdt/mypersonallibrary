9838e8078fa2aa04f822e2efc7d0ab8ef9cd1efc
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-07-25 19:15:17 -0500

    Trying to track activity in user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
   default from: "my.personal.library.helper@gmail.com"
 
   def password_reset_email(user)
+    puts "we are in the user mailer"
     @user = user
     @url = 'http://www.google.com'
     mail(to: @user.email, subject: 'Password Reset')
