35171c3b15acadfd7520c694343f1368ec17742d
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-07-25 19:08:03 -0500

    Fixed missing quotes for GMAIL_USERNAME_DEV environment variable in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,8 @@ Rails.application.configure do
   domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV[GMAIL_USERNAME_DEV],
-  password: ENV[GMAIL_PASSWORD_DEV]
+  user_name: ENV['GMAIL_USERNAME_DEV'],
+  password: ENV['GMAIL_PASSWORD_DEV']
   }
 
 
