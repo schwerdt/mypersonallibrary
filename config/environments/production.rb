72d3c90f510af668b26082856589d3ff08246364
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2015-07-25 22:41:16 -0500

    Testing to see if this helps heroku send emails..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,8 @@ Rails.application.configure do
   domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV['GMAIL_USERNAME_DEV'],
-  password: ENV['GMAIL_PASSWORD_DEV']
+  user_name: 'my.personal.library.helper',
+  password: 'tekabear'
   }
 
 
